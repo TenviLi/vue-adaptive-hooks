f85929648954408c734f12c947887cb9bba26e70
gylidian <gylidian@gmail.com>
2019-11-19 15:30:45 +0800

    feat: add memory hooks

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
 export { default as useNetworkStatus } from "./network";
+export { default as useMemoryStatus } from "./memory";
---
+++ memory.js
@@ -0,0 +1,40 @@
+let unsupported;
+
+// 检查浏览器是否支持 Navigator.deviceMemory
+if (typeof navigator !== "undefined" && "deviceMemory" in navigator) {
+    unsupported = false;
+} else {
+    unsupported = true;
+}
+
+let initialMemoryStatus;
+
+if (!unsupported) {
+    // 检查浏览器是否支持 performance.memory
+    const performanceMemory =
+        "memory" in performance ? performance.memory : null;
+
+    initialMemoryStatus = {
+        deviceMemory: navigator.deviceMemory,
+        // The maximum size of the heap, in bytes, that is available to the context.
+        jsHeapSizeLimit: performanceMemory
+            ? performanceMemory.jsHeapSizeLimit
+            : null,
+        //  The total allocated heap size, in bytes.
+        totalJSHeapSize: performanceMemory
+            ? performanceMemory.totalJSHeapSize
+            : null,
+        // The currently active segment of JS heap, in bytes.
+        usedJSHeapSize: performanceMemory
+            ? performanceMemory.usedJSHeapSize
+            : null
+    };
+} else {
+    initialMemoryStatus = { unsupported };
+}
+
+const useMemoryStatus = () => {
+    return { ...initialMemoryStatus };
+};
+
+export { useMemoryStatus };
