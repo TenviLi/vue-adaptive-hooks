3af0c41fb84037696b749b6f70ee9b1b7ea09690
gylidian <gylidian@gmail.com>
2019-11-19 15:59:04 +0800

    refactor(index.js): add export statement

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 export { default as useNetworkStatus } from "./network";
 export { default as useMemoryStatus } from "./memory";
+export { default as useSaveData } from "./save-data";
+export { default as useHardwareConcurrency } from "./hardware-concurrency";
